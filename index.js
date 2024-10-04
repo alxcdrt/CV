ddbfd03e8c31724e627758a16bde8e84aabc8391
Alexis Coudert <coudert.alex@gmail.com>
2024-10-04 16:37:55 +0200

    add languages selector

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+const defaultLanguage = 'en';
+
+let showLangSelector = false;
+let currentLanguage = defaultLanguage;
+
+const languages = new Map([
+  ['en', 'fi-gb'],
+  ['es', 'fi-es'],
+  ['fr', 'fi-fr']
+]);
+
 window.addEventListener('DOMContentLoaded', (event) => {
     if (document.getElementById('dark-mode-switch').checked)
         toggleDarkMode();
+
+    const url = new URL(window.location.href);
+    let lang = url.searchParams.get('lang');
+
+    if (!lang || !languages.has(lang))
+        lang = defaultLanguage;
+
+    selectLanguage(lang);
+
 });
 
 function toggleDarkMode() {
@@ -18,4 +38,38 @@ function toggleDarkMode() {
 
     document
       .querySelector('footer').classList.toggle('dark');
+}
+
+function toggleSelector() {
+    if (showLangSelector)
+        closeSelector();
+    else
+        openSelector();
+}
+
+function closeSelector() {
+    const selector = document.getElementById('selector-tooltip');
+    selector.style.display = 'none';
+    showLangSelector = false;
+}
+
+function openSelector() {
+    const selector = document.getElementById('selector-tooltip');
+    selector.style.display = 'flex';
+    showLangSelector = true;
+}
+
+function selectLanguage(language) {
+    const selectedLang = document.getElementById('selected-lang');
+  
+    selectedLang.classList.remove(languages.get(currentLanguage));
+    selectedLang.classList.add(languages.get(language));
+  
+    document.querySelector(`section[lang=${currentLanguage}]`).style.display = 'none';
+
+    currentLanguage = language;
+
+    document.querySelector(`section[lang=${currentLanguage}]`).style.display = 'inherit';
+
+    closeSelector();
 }
